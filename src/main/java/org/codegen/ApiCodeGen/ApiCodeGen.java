5bc58ae9e4304f7d33371f5320fd9d920b6296ee
di.garg1 <divyansh.garg@geminisolutions.com>
2022-08-26 15:55:29 +0530

    api commits

--- src/main/java/org/codegen/ApiCodeGen/ApiCodeGen.java
+++ src/main/java/org/codegen/ApiCodeGen/ApiCodeGen.java
@@ -1,4 +1,18 @@
 package org.codegen.ApiCodeGen;
 
+import org.codegen.ApiCodeGen.loader.Classloader;
+import org.codegen.ApiCodeGen.templateHandler.Handlebar;
+
+import static org.codegen.ApiCodeGen.loader.Classloader.readClassName;
+
 public class ApiCodeGen {
+    static Classloader cl=new Classloader();
+    static Handlebar hl=new Handlebar();
+    public static void main(String[] args) {
+      cl.loadClass(readClassName());
+      cl.convertIntoAPIJson();
+      hl.SwaggerYaml();
+
+
+    }
 }
---
+++ src/main/java/org/codegen/ApiCodeGen/templateHandler/Handlebar.java
@@ -0,0 +1,43 @@
+package org.codegen.ApiCodeGen.templateHandler;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+
+public class Handlebar {
+    private static final Logger log = LoggerFactory.getLogger(Handlebar.class);
+    public static void SwaggerYaml()
+    {
+        try
+        {
+            Handlebars handlebars = new Handlebars();
+            Template template = handlebars.compile("\\HandlebarTemplates\\swaggerTemplate");
+            Path path1 = Paths.get("D:\\Intellj Projects\\SwaggerCodeGen\\src\\main\\resources");
+            String filename = "SwaggerYaml" + ".yml";
+            String path = path1 + File.separator + filename;
+            FileReader fileReader = new FileReader("D:\\Intellj Projects\\SwaggerCodeGen\\src\\main\\java\\org\\codegen\\ApiCodeGen\\jsonFiles\\SwaggerJson.json");
+            JSONParser jsonParser = new JSONParser();
+            Object obj = jsonParser.parse(fileReader);
+            FileWriter fileWriter =new FileWriter(path);
+            fileWriter.write(template.apply(obj));
+            fileWriter.close();
+        }
+        catch (Exception e)
+        {
+            log.error("Exception in SwaggerYaml() :{}", e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        SwaggerYaml();
+
+    }
+}
