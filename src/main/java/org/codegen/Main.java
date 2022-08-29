0fd91ef9ce04644f18d356ec6aac8c2d0d4595ae
di.garg1 <divyansh.garg@geminisolutions.com>
2022-08-29 11:12:17 +0530

    main class updates

--- src/main/java/org/codegen/Main.java
+++ src/main/java/org/codegen/Main.java
@@ -1,9 +1,26 @@
 package org.codegen;
 
+import org.codegen.ApiCodeGen.loader.Classloader;
+import org.codegen.ApiCodeGen.templateHandler.Handlebar;
+import org.codegen.JOOQ.PojosGen.EntityClassGen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.codegen.ApiCodeGen.loader.Classloader.readClassName;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    static Classloader cl=new Classloader();
+    static Handlebar hl=new Handlebar();
+    static EntityClassGen entityClassGen =new EntityClassGen();
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        log.info("<------ CodeGen FrameWork Started ------>");
+        entityClassGen.EntityGenerator();
+        cl.loadClass(readClassName());
+        cl.convertIntoAPIJson();
+        hl.SwaggerYaml();
+
 
     }
 }
